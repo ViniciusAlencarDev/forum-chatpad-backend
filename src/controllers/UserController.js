a5bcfef2db445d22b5027294aceb71bd582e8dd5
Vinicius Alencar <viniciusalencardev@gmail.com>
2022-01-03 17:44:57 -0300

    Started controllers

---
+++ src/controllers/UserController.js
@@ -0,0 +1,28 @@
+
+const responseModel = {
+    success: false,
+    data: [],
+    error: []
+}
+
+module.exports = {
+
+    async create(req, res) {
+        const response = {...responseModel}
+
+        return res.json(response)
+    },
+
+    async login(req, res) {
+        const response = {...responseModel}
+
+        return res.json(response)
+    },
+
+    async create(req, res) {
+        const response = {...responseModel}
+
+        return res.json(response)
+    }
+
+}
