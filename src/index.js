4030e347aae5cb9343fdd12d39dd2ed328978a2b
Vinicius Alencar <viniciusalencardev@gmail.com>
2022-01-02 23:03:29 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(express.json())
+app.use(cors(['*']))
+
+const protocol = process.env.PROTOCOL || 'http'
+const ip = require('ip').address()
+const port = process.env.PORT || 3030
+
+app.listen(port, () => console.log(`Server started in http://localhost:${port} or ${protocol}://${ip}:${port}`))
