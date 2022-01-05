23f343bc779aa5dd28f44f42b73a2888c0cda111
Vinicius Alencar <viniciusalencardev@gmail.com>
2022-01-05 00:29:01 -0300

    Initial routes User Controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -38,15 +38,27 @@ module.exports = {
     async login(req, res) {
         const response = {...responseModel}
 
+        const { usernameOrEmail, password } = req.body;
+
         const [, data] = await connection.query(`
-            
+            SELECT id FROM users WHERE 
+                (username='${usernameOrEmail}' OR email='${usernameOrEmail}') AND password='${password}'
+            ORDER BY id DESC LIMIT 1
         `);
+
+        response.success = data.lenth > 0
 
         return res.json(response)
     },
 
     async delete(req, res) {
         const response = {...responseModel}
+
+        const [, affectedRows] = await connection.query(`
+            DELETE FROM users WHERE id=${req.id}
+        `);
+
+        response.success = affectedRows > 0
 
         return res.json(response)
     }
