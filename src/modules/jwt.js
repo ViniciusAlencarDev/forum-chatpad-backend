d79351f19be75f8563a25fc78c3f8af07cf45d4e
Vinicius Alencar <viniciusalencardev@gmail.com>
2022-01-09 13:51:47 -0300

    Verify token

--- src/modules/jwt.js
+++ src/modules/jwt.js
@@ -2,8 +2,34 @@ const jwt = require('jsonwebtoken')
 
 module.exports = {
     
-    async createToken(token) {
+    async createToken(id) {
         return await jwt.sign({ id }, process.env.SECRET)
+    },
+
+    async verifyToken(req, res, next) {
+        const token = req.headers['x-access']
+
+        if(!token) {
+            return res.json({
+                success: false,
+                data: [],
+                error: ['No token']
+            })
+        }
+
+        await jwt.verify(token, process.env.SECRET, function(error, data) {
+            if(error) {
+                return res.json({
+                    success: false,
+                    data: [],
+                    error: ['Error token']
+                })
+            }
+
+            req.id = data.id
+
+            next()
+        })
     }
 
 }
