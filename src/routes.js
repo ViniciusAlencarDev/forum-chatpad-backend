464cb0fd5806b7486762ce455c943badfac83268
Vinicius Alencar <viniciusalencardev@gmail.com>
2022-02-19 11:00:16 -0300

    Basics Routes User Controllers

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
+const { verifyToken } = require('./modules/jwt')
 
 const UserController = require('./controllers/UserController')
 
-router.post('/user/create', UserController.create)
+router.post('/user', UserController.create)
+router.post('/user/login', UserController.login)
+router.patch('/user', verifyToken, UserController.update)
+router.delete('/user', verifyToken, UserController.delete)
 
 module.exports = router
