d9f48583b80db3624219684701d1307c068547bd
Vinicius Alencar <viniciusalencardev@gmail.com>
2022-06-20 08:19:09 -0300

    Change limit list users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -37,7 +37,7 @@ module.exports = {
         const response = {...responseModel}
 
         const [, data] = await connection.query(`
-            SELECT id FROM users LIMIT 100
+            SELECT id FROM users LIMIT 1000
         `);
 
         if(data) {
